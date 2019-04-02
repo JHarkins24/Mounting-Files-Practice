b3ea1681c2db2cbb25ea4a4467f0653c8809be77
JHarkins24 <Harkoe92@hotmail.com>
2019-04-02 14:30:47 -0700

    Add files via upload

---
+++ hellofs/hellofs/build/CMakeFiles/hellofs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellofs.dir/hellofs.c.o"
+  "bin/hellofs.pdb"
+  "bin/hellofs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hellofs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
